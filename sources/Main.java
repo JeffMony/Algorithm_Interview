ea0a5c0076c7f403cb85da1700e563ecbcf95784
JeffMony <jeffmony@163.com>
2020-07-12 02:27:09 +0800

    提交数组、链表、跳表方面算法分析代码4
    Signed-off-by: JeffMony <jeffmony@163.com>

--- sources/Main.java
+++ sources/Main.java
@@ -2,8 +2,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        Solution_019 instance = new Solution_019();
-        int[] arr = new int[]{1, 1, 2};
-        System.out.println(instance.removeDuplicates(arr));
+        Solution_020 instance = new Solution_020();
+        int[] arr = new int[]{1,2};
+        int k = 2;
+        instance.rotate(arr, k);
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
 }
