79ed850aa13035e03251247179d587586f8b227e
JeffMony <jeffmony@163.com>
2020-07-14 02:29:55 +0800

    滑动窗口最大值-->算法分析

--- sources/Main.java
+++ sources/Main.java
@@ -2,9 +2,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        Solution_022 instance = new Solution_022();
-        int[] arr = new int[]{9,8,7,6,5,4,3,2,1,0};
-        int[] res = instance.plusOne(arr);
+        Solution_025 instance = new Solution_025();
+        int[] arr = new int[]{7,2,4};
+        int[] res = instance.maxSlidingWindow(arr, 2);
         for(int i = 0; i < res.length; i++) {
             System.out.println(res[i]);
         }
